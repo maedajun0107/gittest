d3fae9cef9300d945e3e5763c960a471cd337f84
maedajun0107 <maejun0107@icloud.com>
2023-03-09 14:12:46 +0900

    ボタンイベントの追加

--- gittest/mainwindow.cpp
+++ gittest/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <qDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,19 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    qDebug() << "Melon clicked()!";
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    qDebug() << "apple clicked()!";
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    qDebug() << "strawberry clicked()!";
 }
